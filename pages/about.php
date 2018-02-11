d1b5db0d4da95397f3173fafa7d94af56a5ef7a8
HVijay <harshulvijay10@gmail.com>
2018-02-11 18:54:20 +0530

    Add files via upload

---
+++ pages/about.php
@@ -0,0 +1,3 @@
+<h1>About us</h1>
+<div>This website has <b>MatForums &mdash; Forum Software</b> installed.</div><br/>
+<div>&copy; <?php echo date("Y"); ?> MatForums Inc. &mdash; a NineSquad Inc. company.</div>
